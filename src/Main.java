39c667b7ab78efe5656976253f3c31d39f39c07a
julenb <21701719@live.uem.es>
2019-10-17 18:39:58 +0200

    Main class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
-package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,7 +13,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/login.fxml"));
+        Font.loadFont(Main.class.getResource("font/Montserrat-Regular.ttf").toExternalForm(), 20);
+        Font.loadFont(Main.class.getResource("font/Montserrat-Bold.ttf").toExternalForm(), 20);
+        Font.loadFont(Main.class.getResource("font/Orbitron-Bold.ttf").toExternalForm(), 20);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
