2428ca262d77a517ad586d17c86cebe8a209e329
Julen Bujanda <21701719@live.uem.es>
2019-10-24 19:08:27 +0200

    Show login succesful or not dialog

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -3,6 +3,9 @@ package controller;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.Region;
 import model.UsersModel;
 
 public class LoginController {
@@ -24,9 +27,13 @@ public class LoginController {
         String user = txtUser.getText();
         String password = txtPassword.getText();
         if (usersModel.checkPassword(user, password)) {
-
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Succesful login", ButtonType.OK);
+            alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+            alert.show();
         } else {
-
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Incorrect credentials", ButtonType.OK);
+            alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+            alert.show();
         }
     }
 
