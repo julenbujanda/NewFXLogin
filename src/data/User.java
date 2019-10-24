7fa932ecfdd57c2d89426a6923a482b9a53e6cd9
Julen Bujanda <21701719@live.uem.es>
2019-10-24 18:33:08 +0200

    User object with Gson annotations and toString()

---
+++ src/data/User.java
@@ -0,0 +1,53 @@
+package data;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class User {
+
+    @SerializedName("id")
+    @Expose
+    private String id;
+
+    @SerializedName("password")
+    @Expose
+    private String password;
+
+    @SerializedName("role")
+    @Expose
+    private Integer role;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getRole() {
+        return role;
+    }
+
+    public void setRole(Integer role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", password='" + password + '\'' +
+                ", role=" + role +
+                '}';
+    }
+
+}
